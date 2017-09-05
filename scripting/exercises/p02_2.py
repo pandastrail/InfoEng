33a947d1a49709f3ac32e07d50d491bf4faca96f
ksagilop <giovanni.lopez@ksa.ch>
2017-09-05 15:41:51 +0200

    Final revision

--- scripting/exercises/p02_2.py
+++ scripting/exercises/p02_2.py
@@ -10,6 +10,20 @@ Convert degrees Celsius to degrees Fahrenheit
 Write a program that reads a temperature in Celsius from the console 
 and converts it into degrees Fahrenheit.
 
-"""
+*The temperature T in degrees Fahrenheit (°F) 
+is equal to the temperature T in degrees Celsius (°C) times 9/5 plus 32
 
-print('test')
+*http://www.rapidtables.com/convert/temperature/how-celsius-to-fahrenheit.htm
+"""
+# Functions
+def temp(c):
+    f = (c * (9/5)) + 32
+    print('Temp in Fahrenheit is ', "%.1f" % f, 'F') # Float point to 1-digit
+    return f
+
+# Reads a floating point number from keyboard input 
+# and stores it in a variable named “v0”:
+c = float(input('degrees Celsius? '))
+
+# Execute
+temp(c)
