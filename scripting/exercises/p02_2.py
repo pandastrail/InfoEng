511063cfc78f69ee9029fa7b250932e2d568104b
ksagilop <giovanni.lopez@ksa.ch>
2017-09-05 14:10:46 +0200

    First release

---
+++ scripting/exercises/p02_2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 14:02:56 2017
+
+@author: ksagilop
+
+2 Celsius to Fahrenheit
+Convert degrees Celsius to degrees Fahrenheit
+
+Write a program that reads a temperature in Celsius from the console 
+and converts it into degrees Fahrenheit.
+
+"""
+
+print('test')
