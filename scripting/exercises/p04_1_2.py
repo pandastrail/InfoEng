ef69ca534291a8622cc0cece5c51f77cd932a76a
ksagilop <giovanni.lopez@ksa.ch>
2017-09-21 18:06:55 +0200

    Changes to loop for euclidean distance, no scipy

--- scripting/exercises/p04_1_2.py
+++ scripting/exercises/p04_1_2.py
@@ -30,19 +30,54 @@ Or use scipy.spatial.distance.cdist? or pdist?
 # Modules
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # Assigments
 x_point = []
 y_point = []
+d = []
 
-xy_points = (1, 1), (2, 1), (1, 2), (1, 1)
+'''
+xy_points = ({1: {'x': 1, 'y': 1},
+              2: {'x': 2, 'y': 1},
+              3: {'x': 1, 'y': 2},
+              4: {'x': 1, 'y': 1},
+              })
+'''
+    
+xy_points = ({1: {'x':142.492, 'y': 208.536},
+              2: {'x':142.658, 'y': 207.060},
+              3: {'x':143.522, 'y': 205.978},
+              4: {'x':145.009, 'y': 205.546},
+              5: {'x':146.492, 'y': 204.536},
+              6: {'x':147.658, 'y': 203.060},
+              7: {'x':148.522, 'y': 202.978},
+              8: {'x':149.009, 'y': 201.546},
+              })
+    
+#print(xy_points)
 # Distance is equal to 4.41
-for i in range(len(xy_points)):
+for i in range(1, len(xy_points)+1):
     xy_point = xy_points[i]
-    x_point.append(xy_point[0])
-    y_point.append(xy_point[1])
+    #print(xy_point)
+    x_point.append(xy_point['x'])
+    y_point.append(xy_point['y'])
+    
+for i in range(1,len(xy_points),1):
+    print('i: ', i)
+    x = (xy_points[i+1]['x'] - xy_points[i]['x'])**2
+    print(xy_points[i+1]['x'], '-', xy_points[i]['x'], '**2')
+    
+    y = (xy_points[i+1]['y'] - xy_points[i]['y'])**2
+    print(xy_points[i+1]['y'], '-', xy_points[i]['y'], '**2')
+    
+    d_partial = math.sqrt(x+y)
+    d.append(d_partial)
 
-print('x: ', x_point)
-print('y: ', y_point)
+print('partial distances: ', d)
+d_total = sum(d)
+print('total_distance: ', d_total)
+#print('x: ', x_point)
+#print('y: ', y_point)
 plt.plot(x_point, y_point)
 plt.show()
