7725d318f562534a912088ded9b3743eda2c81cd
ksagilop <giovanni.lopez@ksa.ch>
2017-10-17 17:09:37 +0200

    Regex for URLs

---
+++ scripting/exercises/p07_2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 15:50:53 2017
+
+@author: ksagilop
+
+P07 Regex
+
+2. Regular expression for URLs
+
+Based on the script v07_regex_contact_data.py discussed in class:
+
+-Design a regular expression to extract a valid URL and add it to the script
+- Instead of first occurrence, extract all occurrences of phone numbers, 
+emails and URLs
+- Refactor your code to using functions for extracting all phone numbers, email
+addresses and URLs
+- Test your code on email-footer.txt
+
+Hint:look up re.findall()
+"""
+import re
