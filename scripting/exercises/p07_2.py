83f86eb87e665716963ae99b79ec646e8e695574
hase <giovannilopez@hotmail.com>
2017-10-17 23:11:51 +0200

    Regex for phones, emails and URLs

--- scripting/exercises/p07_2.py
+++ scripting/exercises/p07_2.py
@@ -18,5 +18,47 @@ addresses and URLs
 - Test your code on email-footer.txt
 
 Hint:look up re.findall()
+See also https://docs.python.org/2/library/re.html#match-objects
 """
+''' MODULES '''
 import re
+
+''' ASSIGNMENTS '''
+filename = r'/home/hase/Documents/ZHAW/InfoEng/Lectures/Scripting/code/v07_email-footer.txt'
+
+''' FUNCTIONS '''
+def read_file(filename):
+    ''' Load text file to analyze '''
+    try:
+      f = open(filename, 'r')
+    except IOError as detail:
+      print("Cannot open file "+filename, detail)
+      exit()
+    else:
+      with f:  # This is the same as with open(filename, 'r') as f:
+        text = f.read()
+      return text
+
+def phone(text):
+    ''' Find phone numbers, either international or local format '''
+    phones = re.findall(r'\+\d{2} \d{2} \d{3} \d{4}', text) # International
+    phones.append(re.findall(r'0\d{2} \d{3} \d{4}', text)) # Local
+    if phones: 
+      #print(phones, 'of type', type(phones), 'len', len(phones))
+      return True, phones
+    else:
+      return False
+
+def email(text):
+    ''' Find email addresses '''
+    #FIXME email_match = re.search(r'[\w.-]+@[\w.-]+', text)
+    pass
+
+
+''' EXECUTE '''
+text = read_file(filename)
+# Print the text extracted
+print(text)
+# Find phone numbers
+print('Phones found:')
+print(phone(text))
